e28d7f559534b5aa894a6f16a8eeb5adbf5998cf
Hayden Curran <haydenz@gmail.com>
2020-02-15 00:09:31 +1300

    Added dragging of windows, and mouse over focus

--- AsperetaClient/GUIElements/BaseWindow.cs
+++ AsperetaClient/GUIElements/BaseWindow.cs
@@ -16,6 +16,10 @@ namespace AsperetaClient
         private int focusAlpha;
         private int unfocusAlpha;
 
+        private bool mouseDown = false;
+        private int lastMouseDragX = 0;
+        private int lastMouseDragY = 0;
+
         public BaseWindow(string windowName)
         {
             this.Name = windowName;
@@ -45,7 +49,7 @@ namespace AsperetaClient
         {
             if (Hidden) return;
 
-            background?.Render(X + xOffset, Y + yOffset, unfocusAlpha);
+            background?.Render(X + xOffset, Y + yOffset, (this.HasFocus ? focusAlpha : unfocusAlpha));
 
             base.Render(dt, xOffset, yOffset);
         }
@@ -54,7 +58,44 @@ namespace AsperetaClient
         {
             if (Hidden) return false;
 
-            return base.HandleEvent(ev, xOffset, yOffset);
+            bool preventFurtherEvents = base.HandleEvent(ev, xOffset, yOffset);
+            if (preventFurtherEvents)
+                return true;
+
+            switch (ev.type)
+            {
+                case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
+                    if (ev.button.button == SDL.SDL_BUTTON_LEFT && Contains(xOffset, yOffset, ev.button.x, ev.button.y))
+                    {
+                        mouseDown = true;
+                        lastMouseDragX = ev.button.x;
+                        lastMouseDragY = ev.button.y;
+                    }
+                    break;
+                case SDL.SDL_EventType.SDL_MOUSEBUTTONUP:
+                    if (ev.button.button == SDL.SDL_BUTTON_LEFT)
+                    {
+                        mouseDown = false;
+                        lastMouseDragX = 0;
+                        lastMouseDragY = 0;
+                    }
+                    break;
+                case SDL.SDL_EventType.SDL_MOUSEMOTION:
+                    if (mouseDown)
+                    {
+                        // Can't use the xrel since it's relative to desktop, not our window/scaling
+                        this.Rect.x += (ev.motion.x - lastMouseDragX);
+                        this.Rect.y += (ev.motion.y - lastMouseDragY);
+
+                        lastMouseDragX = ev.motion.x;
+                        lastMouseDragY = ev.motion.y;
+                    }
+
+                    this.HasFocus = Contains(xOffset, yOffset, ev.motion.x, ev.motion.y);
+                    break;
+            }
+
+            return false;
         }
     }
 }
